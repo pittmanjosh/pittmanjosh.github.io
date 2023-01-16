baf560cdef52dab133ed1d4dc7b6fb4b30bf54e1
Josh Pittman <code@joshpittman.com>
2023-01-16 17:23:11 -0500

    added bootstrap nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Dropdown,
+  Nav,
+  NavDropdown,
+  NavItem,
+  NavLink,
+  Navbar,
+} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Nav>
+            <Navbar.Brand href="/">Josh Pittman</Navbar.Brand>
+            <Nav.Link href="https://pittmanjosh.github.io/cicdpoker">
+              Poker
+            </Nav.Link>
+            <NavLink href="https://bankify-josh.herokuapp.com/">
+              Bankify
+            </NavLink>
+            <NavDropdown title="Projects">
+              <NavDropdown.Item href="https://pittmanjosh.github.io/cicdpoker">
+                Poker
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://bankify-josh.herokuapp.com/">
+                Bankify
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
